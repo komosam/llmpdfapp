cdd8a28383b101a74b4a6e4cc40c256f34dd19b8
Samson Mwenja <samkomo@Samsons-MBP.attlocal.net>
2023-05-13 15:36:06 -0500

    version1

---
+++ app.py
@@ -0,0 +1,53 @@
+
+from dotenv import load_dotenv
+import streamlit as st
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+def main():
+    load_dotenv()
+    # print(os.getenv('OPENAI_API_KEY'))
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask Daisy 💬")
+
+    pdf = st.file_uploader('upload your pdf', type='pdf')
+
+    if pdf:
+        pdf_reader = PdfReader(pdf)
+        text = ''
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+            #split into chunks 
+            text_splitter = CharacterTextSplitter(
+                separator="\n", 
+                chunk_size=1000,
+                chunk_overlap=200,
+                length_function=len
+            )
+            chunks = text_splitter.split_text(text)
+            #st.write(chunks)
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        user_question = st.text_input("Ask from your pdf")
+        if user_question:
+            #get relevant chunks
+            docs = knowledge_base.similarity_search(user_question)
+            #llm model
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type='stuff')
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=user_question)
+                print(cb)
+            #response = chain.run(input_documents=docs, question=user_question)
+            st.write(response)
+
+if __name__=='__main__':
+    main()
